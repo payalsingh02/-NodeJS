dfcfad4885cd47f794b2a801634df1a50c7d656b
Payal <singhpayal2503@gmail.com>
2021-12-07 22:30:27 -0800

    simple node module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var rect = {
-    perimeter : (x , y) => ( 2*(x+y)),
-    area : (x , y) => ( x * y)
-};
+// var rect = {
+//     perimeter : (x , y) => ( 2*(x+y)),
+//     area : (x , y) => ( x * y)
+// };
+var rect = require('./rectangle');
+
 
 function solveRect (l,b) {
     console.log(" solving for rectangle with l = " + l +" and b =" + b);
