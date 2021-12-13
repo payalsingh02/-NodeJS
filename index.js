460297ab0b35c2e32a08b6a72a57cd80988b04da
Payal Singh <59365910+payal2503@users.noreply.github.com>
2021-12-12 22:34:31 -0800

    Assignment 1 "Node Modules, Express and REST API"
    Node Modules, Express and REST API.,
    node support for HTTP, the express framework and also express router'
    use of express router to implement the support for the REST Api

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
-const dishRouter = require('./routes/dishRouter');
 
 const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+
 const hostname = "localhost";
 const port = 3000;
 
@@ -12,24 +16,13 @@ app.use(morgan('dev'))
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/dishes/:dishId', dishRouter);
 
-// app.get('/dishes/:dishId',(req , res , next) => {
-//     res.end('Will send details of the dish: ' + req.params.dishId + ' to you! ');
-// });
+app.use('/promotions', promoRouter);
+app.use('/promotions/:promoId', promoRouter);
 
-// app.post('/dishes/:dishId',(req , res , next) => {
-//     res.statusCode = 403;
-//     res.end('POST operation not supported on /dishes/ ' + req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId',(req , res , next) => {
-//     res.write('Updating the dish: '+ req.params.dishId + '\n');
-//     res.end('Will update the dish: ' + req.body.name + ' with details ' + req.body.description);
-// }); 
-
-// app.delete('/dishes/:dishId',(req , res , next) => {
-//     res.end('deleting dish! ' + req.params.dishId);
-// }); 
+app.use('/leaders', leaderRouter);
+app.use('/leaders/:leaderId', leaderRouter);
 
 app.use(express.static(__dirname+ '/public'))
 
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -28,6 +28,32 @@ dishRouter.route('/')
 
 .delete((req , res , next) => {
     res.end('deleting all the dishes! ');
+});
+
+
+dishRouter.route('/:dishId')
+.all((req , res , next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type' , 'text/plain');
+    next();
+})
+
+.get((req , res , next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you! ');
+})
+
+.post((req , res , next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/ ' + req.params.dishId);
+})
+
+.put((req , res , next) => {
+    res.write('Updating the dish: '+ req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details ' + req.body.description);
+})
+
+.delete((req , res , next) => {
+    res.end('deleting dish! ' + req.params.dishId);
 }); 
 
 module.exports = dishRouter;
