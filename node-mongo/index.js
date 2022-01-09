cc05dd69d726fc8780b9fe5a19ef0828d1347013
Payal <singhpayal2503@gmail.com>
2022-01-08 20:28:24 -0800

    Express Generator

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoCilent = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -10,25 +11,26 @@ MongoCilent.connect(url,(err , cilent) => {
     console.log("Connected correctly to the Server");
 
     const db = cilent.db(dbname);
-    const collection = db.collection('dishes');
 
-    collection.insertOne({"name":"Uthappizza","description":"testt"} ,(err,result) => {
-        assert.equal(err , null);
+    dboper.insertDocument( db , {name:"Uthappizza" , description :'Test'} , 'dishes' , (result) => {
+        console.log("Insert Documents:\n" , result.ops);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+        dboper.findDocument(db ,'dishes' , (docs) => {
+            console.log("Found Documents", docs);
 
-        collection.find({}).toArray((err , docs)=> {
-            assert.equal(err , null);
+            dboper.updateDocument(db , {name:"Uthappizza"},{description :"Test Updated"} , 'dishes', (result) =>{
+                console.log("Updated Document:\n",result.result);
 
-            console.log('Found:\n');
-            console.log(docs);
+                dboper.findDocument(db ,'dishes' , (docs) => {
+                    console.log("Found Documents", docs);  
 
-            db.dropCollection('dishes' , (err , result) => {
-                assert.equal(err , null);
+                    // db.dropCollection('dishes' , (result) => {
+                    //     console.log("Droped Collection" ,result);
 
-                cilent.close();
+                    cilent.close();
+                    // });
+                });
             });
         });
     });
-})
+});
